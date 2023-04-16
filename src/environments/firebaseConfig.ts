bcc6e07e3f13c788583dc2324013b69b4baaf11a
antran <trannhatan2803@gmail.com>
2023-04-16 09:53:30 +0700

    migrate to angular 8

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -6,12 +6,11 @@ export const FireBaseConfig = {
     storageBucket: "blog-app-3cda4.appspot.com",
     messagingSenderId: "1045419816065", */
 
-       apiKey: "AIzaSyCd0wii8iVgaR8M5zpHraH7VrrHncjIjDE",
-    authDomain: "blog-app-3cda4.firebaseapp.com",
-    databaseURL: "https://blog-app-3cda4.firebaseio.com",
-    projectId: "blog-app-3cda4",
-    storageBucket: "blog-app-3cda4.appspot.com",
-    messagingSenderId: "1045419816065",
-    appId: "1:1045419816065:web:6de6e8117841a8322bc980",
-    
+  apiKey: "AIzaSyB0pdLKmfZj5EgEoZMgmo0yvv81lGmp41Y",
+  authDomain: "my-ecommerce-55995.firebaseapp.com",
+  projectId: "my-ecommerce-55995",
+  storageBucket: "my-ecommerce-55995.appspot.com",
+  messagingSenderId: "72539772748",
+  appId: "1:72539772748:web:1575848a318dddefbf61e9"
+
 };
